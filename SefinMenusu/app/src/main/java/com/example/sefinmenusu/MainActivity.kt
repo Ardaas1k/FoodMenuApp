256d1095eaeda685ce00574368e406c715a666c0
Arda <h.ardaasik@gmail.com>
2019-07-19 09:02:18 +0300

    RecyclerView and Data flow bug
    When data goes trough to Second Act from Main Act crash happens when Main Act load fun loaded

--- SefinMenusu/app/src/main/java/com/example/sefinmenusu/MainActivity.kt
+++ SefinMenusu/app/src/main/java/com/example/sefinmenusu/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sefinmenusu
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,7 @@ import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_list.view.listTxt
 import android.content.SharedPreferences
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.PersistableBundle
 import java.io.FileInputStream
 import java.io.ObjectInputStream
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         arrPackage = ArrayList()
 
@@ -59,8 +61,9 @@ class MainActivity : AppCompatActivity() {
 
         backPageBtn.setOnClickListener {
             val intent = Intent(this, SefinMenusuKami2::class.java)
-            val gson = Gson()
-            intent.putExtra("obj", gson.toJson(arrPackage))
+           /* val bundle = Bundle()
+            bundle.putSerializable("arrPackage", arrPackage)
+            intent.putExtras(bundle) */
             startActivity(intent)
 
         }
@@ -77,92 +80,76 @@ class MainActivity : AppCompatActivity() {
             println("You Click Save Button")
         }
 
-  /*      if(savedInstanceState!=null){
-            arrPackage = savedInstanceState.getSerializable("savedItems") as ArrayList<items>?
-            println(arrPackage)
-          // list_item.layoutManager = LinearLayoutManager(this)
-            //list_item.adapter = foodAdapter(arrPackage!!, this)
 
-        }
-*/
-
-    }
-
-
-    class foodAdapter(val items: ArrayList<items>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.foodTxt.text = items.get(position).toString()
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_list, parent, false))
-        }
-
-        override fun getItemCount(): Int {
-            return items.size
-        }
-    }
-    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-        val foodTxt = view.listTxt
-    }
-
-    private fun loadPrefData() {
-        val sharedPreferences = getSharedPreferences("key", Context.MODE_PRIVATE)
-        val gson = Gson()
-        val json = sharedPreferences.getString("key", "")
-        val type = object : TypeToken<ArrayList<items>>() {
-        }.type
-        arrPackage = gson.fromJson(json, type)
-        println(arrPackage)
-    }
-
-    private fun insertItem(newFood: items) {
-        arrPackage?.add(newFood)
-
-    }
-
-    private fun savePrefData(){
-
-        val ModelArrayList = arrPackage
-
-        val shref: SharedPreferences
-        val editor: SharedPreferences.Editor
-
-        shref = getSharedPreferences("key", Context.MODE_PRIVATE)
-
-        val gson = Gson()
-        val json = gson.toJson(ModelArrayList)
-
-        editor = shref.edit()
-        editor.remove("key").apply()
-        editor.putString("key", json)
-        println(json)
-        editor.apply()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        val fis = FileInputStream("savedItems")
-        val ois = ObjectInputStream(fis)
-        arrPackage = ois.readObject() as ArrayList<items>
-        outState.putSerializable("savedItems",arrPackage)
-
-       // outState.putParcelableArrayList("savedItems",arrPackage)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
         if (savedInstanceState != null) {
             arrPackage = savedInstanceState.getSerializable("savedItems") as ArrayList<items>?
+            list_item.layoutManager = LinearLayoutManager(this)
+            list_item.adapter = foodAdapter(arrPackage!!, this)
+
         }
-        println(arrPackage)
     }
 
 
+        class foodAdapter(val items: ArrayList<items>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                holder.foodTxt.text = items.get(position).toString()
+            }
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                return ViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_list, parent, false))
+            }
+
+            override fun getItemCount(): Int {
+                return items.size
+            }
+        }
+
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val foodTxt = view.listTxt
+        }
+
+        private fun loadPrefData() {
+            val sharedPreferences = getSharedPreferences("key", Context.MODE_PRIVATE)
+            val gson = Gson()
+            val json = sharedPreferences.getString("key", "")
+            val type = object : TypeToken<ArrayList<items>>() {
+            }.type
+            arrPackage = gson.fromJson(json, type)
+            println(arrPackage)
+        }
+
+        private fun insertItem(newFood: items) {
+            arrPackage?.add(newFood)
+
+        }
+
+        private fun savePrefData() {
+
+            val ModelArrayList = arrPackage
+
+            val shref: SharedPreferences
+            val editor: SharedPreferences.Editor
+
+            shref = getSharedPreferences("key", Context.MODE_PRIVATE)
+
+            val gson = Gson()
+            val json = gson.toJson(ModelArrayList)
+
+            editor = shref.edit()
+            editor.remove("key").apply()
+            editor.putString("key", json)
+            println(json)
+            editor.apply()
+        }
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            outState.putSerializable("savedItems", arrPackage)
+        }
 
 
+    }
 
-}
 
 
 
